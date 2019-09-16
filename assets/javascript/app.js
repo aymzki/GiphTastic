4c4874173818cadd56d3fa5fddc5e1f0aed6fad4
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-09-16 12:07:18 -0400

    added function to show the gifs with ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,46 @@
- var ghibliMovies = ["Kiki's Delivery Service", "My Neighbor Totoro", "Spirited Away", "Princess Mononoke", "Nausicaa", "Howl's Moving Castle", "The Wind Rises"];
-  
- function makeButtons() {
-     $("#buttonsDiv").empty(); //empties div to avoid duplicates
+var anime = ["Kiki's Delivery Service", "My Neighbor Totoro", "Spirited Away", "Princess Mononoke", "Howl's Moving Castle", "Attack on Titan", "Naruto", "Durarara", "Sailor Moon", "Dragon Ball Z"];
+
+function makeButtons() {
+    $("#buttonsDiv").empty(); //empties div to avoid duplicates
 
     //make buttons with attributes for each item in array
-    for (var i = 0; i < ghibliMovies.length; i++) {
+    for (var i = 0; i < anime.length; i++) {
         var button = $("<button>");
-        button.html(ghibliMovies[i]);
+        button.html(anime[i]);
         button.addClass("btn btn-outline-secondary");
-        button.attr("movie-title", ghibliMovies[i]);
-        button.text(ghibliMovies[i]);
-		$("#buttonsDiv").append(button);
+        button.attr("anime-title", anime[i]);
+        button.text(anime[i]);
+        $("#buttonsDiv").append(button);
     }
- }
- 
+}
+//what happens when you click the button with title
+$("button").on("click", function () {
+    var thisAnime = $(this).attr("anime-title");
+    console.log(thisAnime);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisAnime + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    // ajax call gets and returns the response object from query url
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Storing an array of results in the results variable
+        var results = response.data;
+        //loop to ceate a div that contains image and filters ratings
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var animeImage = $("<img>");
+                animeImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(animeImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+            }
+        }
+    });
+});
+
 makeButtons();
-
- 
-
-// $("button").on("click", function() {
-//     // In this case, the "this" keyword refers to the button that was clicked
-//     var person = $(this).attr("data-movie");
-
-//     // Constructing a URL to search Giphy for the name of the person who said the quote
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//       person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
-//     // ajax call
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     })
-//       // After the data comes back from the API
-//       .then(function(response) {
-//         // Storing an array of results in the results variable
-//         var results = response.data;
-
-//         // Looping the result item
-//         for (var i = 0; i < results.length; i++) {
-
-//           // Only taking action if the photo has an appropriate rating
-//           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            
-//             //create div for the gif
-//             var gifDiv = $("<div>");
-
-//             //store the result item's rating
-//             var rating = results[i].rating;
-
-//             // paragraph tag with the result item's rating
-//             var p = $("<p>").text("Rating: " + rating);
-
-//             // create an image tag
-//             var ghibliImage = $("<img>");
-
-//             // give the image tag an src attribute of a proprty pulled off the
-//             // result item
-//             ghibliImage.attr("src", results[i].images.fixed_height.url);
-
-//             // Appending the paragraph and personImage we created to the "gifDiv" div we created
-//             gifDiv.append(p);
-//             gifDiv.append(ghibliImage);
-
-//             // Prepend gifDiv to the "#gifs-appear-here" div in the HTML
-//             $("#gifs-appear-here").prepend(gifDiv);
-//           }
-//         }
-//       });
-//   });
-//
